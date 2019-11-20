851d2eca8a63b03adf3c6233409695ddf0a3a979
amshalev <57673782+amshalev@users.noreply.github.com>
2019-11-20 15:05:10 +0200

    add GZipEventHubIngestion
    Ingest GZip compressed data via dynamic routing

--- EventHubSampleData/EventHub2/Program.cs
+++ EventHubSampleData/EventHub2/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.ServiceBus.Messaging;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Text;
 using System.Threading;
 using System.Linq;
@@ -16,6 +18,7 @@ namespace SendSampleData
         static void Main(string[] args)
         {
             EventHubIngestion();
+            //GZipEventHubIngestion();
         }
 
         static void EventHubIngestion()
@@ -39,6 +42,7 @@ namespace SendSampleData
                     // eventData.Properties.Add("Table", "TestTable");
                     // eventData.Properties.Add("IngestionMappingReference", "TestMapping");
                     // eventData.Properties.Add("Format", "json");
+                    // eventData.Properties.Add("Compression", "None"); // default
                     eventHubClient.Send(eventData);
                 }
                 catch (Exception exception)
@@ -52,6 +56,59 @@ namespace SendSampleData
 
                 Thread.Sleep(10000);
             }
+        }
+        
+        // Ingest GZip compressed data via dynamic routing
+        static void GZipEventHubIngestion()
+        {
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, eventHubName);
+            int counter = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                int recordsPerMessage = 3;
+                try
+                {
+                    List<string> records = Enumerable
+                        .Range(0, recordsPerMessage)
+                        .Select(recordNumber => $"{{\"timeStamp\": \"{DateTime.UtcNow.AddSeconds(100 * counter)}\", \"name\": \"{$"name {counter}"}\", \"metric\": {counter + recordNumber}, \"source\": \"EventHubMessage\"}}")
+                        .ToList();
+                    string recordString = string.Join(Environment.NewLine, records);
+
+                    EventData eventData = new EventData(GZipCompressString(recordString));
+                    Console.WriteLine($"sending message {counter}");
+                    eventData.Properties.Add("Compression", "GZip");
+                    // Optional "dynamic routing" properties for the database, table, and mapping you created. 
+                    // eventData.Properties.Add("Table", "TestTable");
+                    // eventData.Properties.Add("IngestionMappingReference", "TestMapping");
+                    // eventData.Properties.Add("Format", "json");
+                    eventHubClient.Send(eventData);
+                }
+                catch (Exception exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0} > Exception: {1}", DateTime.Now, exception.Message);
+                    Console.ResetColor();
+                }
+
+                counter += recordsPerMessage;
+
+                Thread.Sleep(10000);
+            }
+        }
+        
+        private static byte[] GZipCompressString(string stringToCompress)
+        {
+            byte[] compressed;
+
+            using (var result = new MemoryStream())
+            {
+                using (var compressionStream = new GZipStream(result, CompressionMode.Compress))
+                {
+                    compressionStream.Write(Encoding.UTF8.GetBytes(stringToCompress), 0, stringToCompress.Length);
+                }
+                compressed = result.ToArray();
+            }
+            return compressed;
         }
     }
 }
